7d3d4d6d43327626fd85b5c40d666632dd11cc30
Vladislav Cernega <vladislav@walkcat.net>
2023-03-14 19:33:03 +0000

    Commit on 2023-03-14 19:33:03

---
+++ app.py
@@ -0,0 +1,9 @@
+# Store this code in 'app.py' file
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+
+app = Flask(__name__)
