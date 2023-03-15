5a902dc48acb633e611c8af880db60f32e08fdca
Vladislav Cernega <vladislav@walkcat.net>
2023-03-15 14:08:46 +0000

    Commit on 2023-03-15 14:08:46

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ app.config['MYSQL_PASSWORD'] = 'Pass123!'
 app.config['MYSQL_DB'] = 'main_db'
 
 mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    pass
