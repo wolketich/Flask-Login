6bdcd066e51c8e73ecccd6fcf65b5704b6516122
Vladislav Cernega <vladislav@walkcat.net>
2023-03-17 08:59:29 +0000

    Commit on 2023-03-17 08:59:29

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ mysql = MySQL(app)
 
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
-def login():1
+def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
